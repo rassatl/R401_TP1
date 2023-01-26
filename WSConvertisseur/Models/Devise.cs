a5a6efac6398a8ef850b117f4378e3b3125cfc66
Lou RASSAT <Lou.Rassat@etu.univ-smb.fr>
2023-01-26 08:56:00 +0100

    Ajout du modèle

---
+++ WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,40 @@
+﻿namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+        public Devise(int id, string? nomDevise, double taux)
+        {
+            this.Id = id;
+            this.NomDevise = nomDevise;
+            this.Taux = taux;
+        }
+
+        public Devise()
+        {
+        }
+
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private string? nomDevise;
+
+        public string? NomDevise
+        {
+            get { return nomDevise; }
+            set { nomDevise = value; }
+        }
+
+        private double taux;
+
+        public double Taux
+        {
+            get { return taux; }
+            set { taux = value; }
+        }
+    }
+}

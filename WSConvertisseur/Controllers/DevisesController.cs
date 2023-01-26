6e10bc8c4cc83b08ebd4423fbf9f855224934b40
Lou RASSAT <Lou.Rassat@etu.univ-smb.fr>
2023-01-26 09:46:32 +0100

    Codage Post

--- WSConvertisseur/Controllers/DevisesController.cs
+++ WSConvertisseur/Controllers/DevisesController.cs
@@ -43,8 +43,14 @@ namespace WSConvertisseur.Controllers
 
         // POST api/<DevisesController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult<Devise> Post([FromBody] Devise devise)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            listDevises.Add(devise);
+            return CreatedAtRoute("GetDevise", new { id = devise.Id }, devise);
         }
 
         // PUT api/<DevisesController>/5

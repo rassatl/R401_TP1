f455b8185c759a9605ecaa9b2747298b8cb7db50
Lou RASSAT <Lou.Rassat@etu.univ-smb.fr>
2023-01-27 14:16:22 +0100

    code testefini

--- WSConvertisseurTests/Controllers/DevisesControllerTests.cs
+++ WSConvertisseurTests/Controllers/DevisesControllerTests.cs
@@ -28,7 +28,7 @@ namespace WSConvertisseur.Controllers.Tests
             Assert.AreEqual(new Devise(1, "Dollar", 1.08), (Devise?)result.Value, "Devises pas identiques"); //Test de la devise récupérée
         }
         [TestMethod]
-        public void GetById_NotExistingIdPassed_Returns404()
+        public void GetById_NotExistingIdPassed_ReturnsRightItem()
         {
             // Arrange
             DevisesController controller = new DevisesController();
@@ -47,7 +47,6 @@ namespace WSConvertisseur.Controllers.Tests
             // Arrange
             DevisesController controller = new DevisesController();
             // Act
-
             var result = controller.GetAll();
             // Assert
             Assert.IsInstanceOfType(result, typeof(IEnumerable<Devise>), "Pas un IEnumerable"); // Test du type de retour
@@ -61,7 +60,6 @@ namespace WSConvertisseur.Controllers.Tests
             // Arrange
             DevisesController controller = new DevisesController();
             // Act
-
             var result = controller.Post(new Devise(4, "gabriel", 15));
             // Assert
 
@@ -70,7 +68,7 @@ namespace WSConvertisseur.Controllers.Tests
             Assert.IsInstanceOfType(result.Result, typeof(CreatedAtRouteResult), "pas de type CreatedAtRouteResult"); //Test de l'erreur
             CreatedAtRouteResult routeResult = (CreatedAtRouteResult)result.Result;
             Assert.AreEqual(StatusCodes.Status201Created, routeResult.StatusCode, "pas un StatusCode");
-            Assert.AreEqual(routeResult.Value, new Devise(4, "gabriel", 15), "Pas la bonne valeur");
+            Assert.AreEqual(routeResult.Value, new Devise(4, "gabriel", 15), "La bonne valeur");
         }
 
         [TestMethod]
@@ -80,7 +78,6 @@ namespace WSConvertisseur.Controllers.Tests
             // Arrange
             DevisesController controller = new DevisesController();
             // Act
-
             var result = controller.Post(new Devise(4, null, 15));
             // Assert
 
@@ -90,6 +87,83 @@ namespace WSConvertisseur.Controllers.Tests
             CreatedAtRouteResult routeResult = (CreatedAtRouteResult)result.Result;
             Assert.AreEqual(StatusCodes.Status201Created, routeResult.StatusCode, "pas un StatusCode");
             Assert.AreEqual(routeResult.Value, new Devise(4, null, 15), "Pas la bonne valeur");
+        }
+
+        [TestMethod]
+        public void Put_InvalidUpdate_ReturnsBadRequest()
+        {
+
+            // Arrange
+            DevisesController controller = new DevisesController();
+            // Act
+            var result = controller.Put(1,new Devise(666, "lou", 15));
+            // Assert
+
+
+            Assert.IsInstanceOfType(result, typeof(ActionResult), "Pas un ActionResult"); // Test du type de retour
+            Assert.IsInstanceOfType(result, typeof(BadRequestResult), "pas de type BadRequestResult"); //Test de l'erreur
+            BadRequestResult routeResult = (BadRequestResult)result;
+            Assert.AreEqual(StatusCodes.Status400BadRequest, routeResult.StatusCode, "pas un StatusCode");
+        }
+
+        [TestMethod]
+        public void Put_InvalidUpdate_ReturnsNotFound()
+        {
+            // Arrange
+            DevisesController controller = new DevisesController();
+            // Act
+            var result = controller.Put(100, new Devise(100, "lou", 15));
+            // Assert
+
+
+            Assert.IsInstanceOfType(result, typeof(ActionResult), "Pas un ActionResult"); // Test du type de retour
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult), "pas de type NotFoundResult"); //Test de l'erreur
+            NotFoundResult routeResult = (NotFoundResult)result;
+            Assert.AreEqual(StatusCodes.Status404NotFound, routeResult.StatusCode, "pas un StatusCode");
+        }
+
+        [TestMethod]
+        public void Put_InvalidValidate_ReturnsNoContent()
+        {
+            // Arrange
+            DevisesController controller = new DevisesController();
+            // Act
+            var result = controller.Put(1, new Devise(1, "lou", 2));
+            // Assert
+
+
+            Assert.IsInstanceOfType(result, typeof(ActionResult), "Pas un ActionResult"); // Test du type de retour
+            Assert.IsInstanceOfType(result, typeof(NoContentResult), "pas de type BadRequestResult"); //Test de l'erreur
+            NoContentResult routeResult = (NoContentResult)result;
+            Assert.AreEqual(StatusCodes.Status204NoContent, routeResult.StatusCode, "pas un StatusCode");
+        }
+        
+        [TestMethod]
+        public void Delete_NotOK_ReturnsNotFound()
+        {
+            // Arrange
+            DevisesController controller = new DevisesController();
+            // Act
+            var result = controller.Delete(10);
+            // Assert
+
+            Assert.IsInstanceOfType(result, typeof(ActionResult<Devise>), "Pas un ActionResult"); // Test du type de retour
+            NotFoundResult routeResult = (NotFoundResult)result.Result;
+            Assert.AreEqual(StatusCodes.Status404NotFound, routeResult.StatusCode, "pas un StatusCode");
+        }
+
+        [TestMethod]
+        public void Delete_Ok_ReturnsRightItem()
+        {
+            // Arrange
+            DevisesController controller = new DevisesController();
+            // Act
+            var result = controller.Delete(1);
+            // Assert
+
+            Assert.IsInstanceOfType(result, typeof(ActionResult<Devise>), "Pas un ActionResult"); // Test du type de retour
+            NoContentResult routeResult = (NoContentResult)result.Result;
+            Assert.AreEqual(StatusCodes.Status204NoContent, routeResult.StatusCode, "pas un StatusCode");
         }
     }
 }
